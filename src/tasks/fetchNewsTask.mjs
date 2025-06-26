11e686efeef248fb0f3c208a9d1b11218442d38c
Caipei FAN <caipei@Caipeis-MacBook-Air.local>
2025-06-26 10:29:24 +0800

    change strategy

--- src/tasks/fetchNewsTask.mjs
+++ src/tasks/fetchNewsTask.mjs
@@ -5,39 +5,41 @@ import { getTopNews } from "../getNews.mjs";
 
 class FetchNewsTask {
     static taskId;
-    static currentNewsTypeIndex = 0;
+    // static currentNewsTypeIndex = 0;
     static startTask = () => {
         if(this.taskId){
             console.log('fetch news task already started');
             return;
         }
         const task = async () => {
-            getTopNews(NEWS_TYPES[this.currentNewsTypeIndex]).then((data) => {
-                    console.log('news: ', data);
-                    if(!data?.articles?.length){
-                        return;
-                    }
-
-                    DBManager.saveNews(data.articles?.map(item => {
-                        return {
-                            title: item.title,
-                            description: item.description,
-                            content: item.content,
-                            image: item.image,
-                            url: item.url,
-                            timestamp: new Date(item.publishedAt).getTime(),
-                            type: NEWS_TYPES[this.currentNewsTypeIndex],
-                            published: false,
+            NEWS_TYPES.forEach((type) => {
+                getTopNews(type).then((data) => {
+                        console.log('news: ', data);
+                        if(!data?.articles?.length){
+                            return;
                         }
-                    })
-                ).finally(() => {
-                    this.currentNewsTypeIndex = (this.currentNewsTypeIndex + 1) % NEWS_TYPES.length;
+
+                        DBManager.saveNews(data.articles?.map(item => {
+                            return {
+                                title: item.title,
+                                description: item.description,
+                                content: item.content,
+                                image: item.image,
+                                url: item.url,
+                                timestamp: new Date(item.publishedAt).getTime(),
+                                type: type,
+                                published: false,
+                            }
+                        })
+                    ).finally(() => {
+                        // this.currentNewsTypeIndex = (this.currentNewsTypeIndex + 1) % NEWS_TYPES.length;
+                    });
                 });
-            });
+            })
         }
 
         task();
-        const intervalTaskId = setInterval(task, 20 * 60 *1000 /**20 mins */);
+        const intervalTaskId = setInterval(task, 60 * 60 *1000 /**1 hour */);
 
         this.taskId = intervalTaskId;
     }
--- src/tasks/publishNewsTask.mjs
+++ src/tasks/publishNewsTask.mjs
@@ -26,7 +26,7 @@ class PublishNewsTask {
         }
 
         task();
-        const intervalTaskId = setInterval(task, 1 * 60 *1000 /**1 mins */);
+        const intervalTaskId = setInterval(task, 2 * 60 *1000 /**2 mins */);
 
         this.taskId = intervalTaskId;
     }
